488ced80fe854a8c8c847e19fc17d4683e064e0f
emilia98 <emilia_nedialkova@abv.bg>
2020-08-03 00:13:39 +0300

    added Destructuring Demo

---
+++ Demo/Next Generation JavaScript/Destructuring/array_destructuring.js
@@ -0,0 +1,14 @@
+[a, b] = ['Hello', 'Max'];
+console.log(a);
+console.log(b);
+
+console.log(('-').repeat(10));
+
+const numbers = [1, 2, 3];
+[num1, num2] = numbers;
+console.log(num1, num2);
+
+console.log(('-').repeat(10));
+
+[num1, , num3] = numbers;
+console.log(num1, num3);
---
+++ Demo/Next Generation JavaScript/Destructuring/object_destructuring.js
@@ -0,0 +1,3 @@
+let { name } = { name: 'Max', age: 28}
+console.log(name);
+// console.log(age); // undefined
