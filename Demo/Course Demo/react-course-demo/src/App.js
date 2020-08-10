370e257ea698dc9b091899d79987cd09bcc08ee8
emilia98 <emilia_nedialkova@abv.bg>
2020-08-10 12:01:57 +0300

    init Course Demo

---
+++ Demo/Course Demo/react-course-demo/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Person from './Person/Person';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Person />
+      <Person />
+      <Person />
+    </div>
+  );
+}
+
+export default App;
---
+++ Demo/Course Demo/react-course-demo/src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const person = () => {
+    return (
+        <p>Hi, I'm a Person and I'm {Math.floor(Math.random() * 100)}!</p>
+    )
+}
+
+export default person;
