efce3806a379bc2c91a3d07c940a7d91a310fd8d
emilia98 <emilia_nedialkova@abv.bg>
2020-08-14 10:34:05 +0300

    added Understanding State Demo

--- Demo/Course Demo/react-course-demo/src/App.js
+++ Demo/Course Demo/react-course-demo/src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Person name="Emilia" age="22"/>
-      <Person name="Max" age="28"/>
-      <Person name="Manu" age="29">My hobbies: Racing</Person>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Emilia', age: 22 },
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 }
+    ]
+  }
+
+  
+  render() {
+    return (
+      <div className="App">
+        <button>Switch Names</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My hobbies: Racing</Person>
+      </div>
+    )
+  }
 }
 
 export default App;
